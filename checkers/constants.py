3010f7e30a0d9437c5b7fff84c0cd7df5c4c6337
Labychu <inzector0102@gmail.com>
2020-09-26 17:18:59 +0000

    Add checker module    and draw the board

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH // COLS // 1.5
+
+RED = (255, 0, 0)
+BLK = (0, 0, 0)
+WHT = (255, 255, 255)
+BLUE = (0, 0, 255)
+CLR1 = (237, 191, 104)
+CLR2 = (122, 64, 20)
+BG = (43, 64, 48)
